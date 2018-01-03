ad024e5b8c3ddddcb59223288986a9640bfb0c18
tehmoose <megadxr@gmail.com>
2018-01-04 04:45:11 +0600

    Refactor and modify js code to generate Netplan configuration file.

--- js/wizard.js
+++ js/wizard.js
@@ -21,6 +21,16 @@ function validateNetmask(netmask)
   return (false);
 }
 
+function convertNetmaskToCIDR(netmask) {
+  var maskNodes = netmask.match(/(\d+)/g);
+  var cidr = 0;
+  for(var i in maskNodes)
+  {
+    cidr += (((maskNodes[i] >>> 0).toString(2)).match(/1/g) || []).length;
+  }
+  return cidr;
+}
+
 
 function saveFile(filename, data) {
   var blob = new Blob([data], {type: 'text/csv'});
@@ -37,7 +47,6 @@ function saveFile(filename, data) {
   }
 }
 
-$('input[id=screenlyv1]').prop('checked', 'checked');
 $('#wifi-fields').hide();
 $('#wired-fields').hide();
 $('#dns-fields').hide();
@@ -47,6 +56,23 @@ $('#wifi-no-dhcp-fields').hide();
 $('#wired-no-dhcp-fields').hide();
 $('input[name=wifidhcp]').prop('checked', 'checked');
 $('input[name=wireddhcp]').prop('checked', 'checked');
+$('input[id=screenlyv1]').prop('checked', 'checked');
+
+$('input[name=screenlyversion]').change(function() {
+  console.log("Screenly version changed");
+  var ntp = document.getElementById("ntp")
+  if($('input[id=screenlyv1]').is(':checked'))
+  {
+    console.log("Screenly v1 is selected");
+    ntp.disabled = false;
+  }
+  else
+  {
+    console.log("Screenly v2 is selected");
+    ntp.disabled = true;
+    $('#ntp-fields').hide();
+  }
+});
 
 $('input[name=wifi]').change(function() {
   if($(this).is(':checked'))
@@ -148,136 +174,226 @@ $('#generateconfig').click(function()
   $('.help-block').remove();
   $('#common_errors').hide();
 
+  // Validate and get values from user
+
   var validation_errors = [];
 
-  var configStr = "";;
-  if($('#wifi').is(":checked") == false && $('#wired').is(":checked") == false)
-  {
+  var v1Str = "";
+  var v2Str = "network:\r\n  version: 2\r\n";
+
+  var has_wifi = $('#wifi').is(":checked");
+  var wifi_ssid = $('#ssid').val();
+  var wifi_hidden_ssid = $('#hiddenssid').is(":checked");
+  var wifi_passphrase = $('#passphrase').val();
+  var wifi_dhcp = $('#wifidhcp').is(":checked");
+  var wifi_ip = $('#wifiip').val();
+  var wifi_gw = $('#wifigateway').val();
+  var wifi_mask = $('#wifinetmask').val();
+
+  var has_wired = $('#wired').is(":checked");
+  var wired_dhcp = $('#wireddhcp').is(":checked");
+  var wired_ip = $('#wiredip').val();
+  var wired_gw = $('#wiredgateway').val();
+  var wired_mask = $('#wirednetmask').val();
+
+  var has_custom_ntp = $('#ntp').is(":checked");
+  var ntp_server_list = [];
+
+  var has_custom_dns = $('#dns').is(":checked");
+  var dns_server_list = [];
+
+  if(has_wifi == false && has_wired == false) {
     validation_errors.push({'key':'common_error' , 'message': 'You must configure either a WiFi or wired interface.'});
   }
 
-  if($('#wifi').is(":checked") == true)
-  {
-    if($('#ssid').val() == "")
-    {
-      validation_errors.push({'key': 'ssid' , 'message':'SSID is required'});
-
-    }
-
-    configStr = configStr + "[wlan0]";
-    configStr = configStr + "\r\n";
-    configStr = configStr + "ssid=" + $('#ssid').val() + "\r\n";
-    if($('#passphrase').val() != "")
-    configStr = configStr + "passphrase=" + $('#passphrase').val() + "\r\n";
-    if($('#wifidhcp').is(":checked") == true)
-    {
-      configStr = configStr + "mode=dhcp" + "\r\n";
-    }
-    else {
-      if(!validateIPaddress($('#wifiip').val()))
-      {
-        validation_errors.push({'key': 'wifiip' ,'message' : 'You have entered an invalid IP address.'});
-      }
-      if(!validateIPaddress($('#wifigateway').val()))
-      {
-        validation_errors.push({'key': 'wifigateway' ,'message' : 'You have entered an invalid IP address.'});
-      }
-      if(!validateNetmask($('#wifinetmask').val()))
-      {
-        validation_errors.push({'key': 'wifinetmask' ,'message' : 'You have entered an invalid netmask.'});
-      }
-      configStr = configStr + "ip=" + $('#wifiip').val() + "\r\n";
-      configStr = configStr + "netmask=" + $('#wifinetmask').val() + "\r\n";
-      configStr = configStr + "gateway=" + $('#wifigateway').val() + "\r\n";
-    }
-
-    if($('#hiddenssid').is(":checked") == true)
-    {
-      configStr = configStr + "hidden_ssid=true" + "\r\n";
-    }
-    configStr = configStr + "\r\n";
-  }
-
-  if($('#wired').is(":checked") == true)
-  {
-    configStr = configStr + "[eth0]";
-    configStr = configStr + "\r\n";
-    if($('#wireddhcp').is(":checked") == true)
-    {
-      configStr = configStr + "mode=dhcp" + "\r\n";
-    }
-    else {
-      if(!validateIPaddress($('#wiredip').val()))
-      {
-        validation_errors.push({'key': 'wiredip' ,'message' : 'You have entered an invalid IP address.'});
-      }
-      if(!validateIPaddress($('#wiredgateway').val()))
-      {
-        validation_errors.push({'key': 'wiredgateway' ,'message' : 'You have entered an invalid IP address.'});
-      }
-      if(!validateNetmask($('#wirednetmask').val()))
-      {
-        validation_errors.push({'key': 'wirednetmask' ,'message' : 'You have entered an invalid netmask.'});
-      }
-      configStr = configStr + "ip=" + $('#wiredip').val() + "\r\n";
-      configStr = configStr + "netmask=" + $('#wirednetmask').val() + "\r\n";
-      configStr = configStr + "gateway=" + $('#wiredgateway').val() + "\r\n";
-    }
-  }
-
-  if($('#ntp').is(":checked") == true || $('#dns').is(":checked") == true)
-  {
-    configStr = configStr + "\r\n";
-    configStr = configStr + "[generic]";
-    configStr = configStr + "\r\n";
-  }
-
-  if($('#ntp').is(":checked") == true)
-  {
-    configStr = configStr + "ntp=";
-    for(var i = 0; i < $('[id^="ntp_server_"]').length; i++)
-    {
-      var ntpid = "ntp_server_" + (i+1);
+  if(has_custom_ntp == true) {
+    for(var i = 0; i < $('[id^="ntp_server_"]').length; i++) {
       var itm = $('[id^="ntp_server_"]')[i];
-      if(validateIPaddress(itm.value) || validateFQDN(itm.value) )
-      {
-        if(i > 0)
-        {
-          configStr = configStr + ",";
-        }
-        configStr = configStr + itm.value;
+      var ntpid = "ntp_server_" + (i+1);
+      if(validateIPaddress(itm.value) || validateFQDN(itm.value) ) {
+        ntp_server_list.push(itm.value);
       }
       else {
         validation_errors.push({ "key": ntpid, 'message': 'You have entered an invalid IP address/FQDN' });
       }
-
     }
-    configStr = configStr + "\r\n";
   }
 
-
-
-  if($('#dns').is(":checked") == true)
-  {
-    configStr = configStr + "dns=";
-
-    for(var i = 0; i < $('[id^="dns_server_"]').length; i++)
-    {
+  if(has_custom_dns == true) {
+    for(var i = 0; i < $('[id^="dns_server_"]').length; i++) {
       var itm = $('[id^="dns_server_"]')[i];
       var dnsid = "dns_server_" + (i+1);
-      if(!validateIPaddress(itm.value))
-      {
+      if(validateIPaddress(itm.value)) {
+        dns_server_list.push(itm.value);
+      }
+      else {
         validation_errors.push({ "key": dnsid, 'message': 'You have entered an invalid IP address' });
       }
-      if(i > 0)
-      {
-        configStr = configStr + ",";
-      }
-      configStr = configStr + itm.value;
     }
-    configStr = configStr + "\r\n";
   }
 
+  if(has_wifi == true) {
+    if(wifi_ssid == "")
+      validation_errors.push({'key': 'ssid' , 'message':'SSID is required'});
+
+    if(wifi_dhcp == false) {
+      if(!validateIPaddress(wifi_ip)) {
+        validation_errors.push({'key': 'wifiip' ,'message' : 'You have entered an invalid IP address.'});
+      }
+      if(!validateIPaddress(wifi_gw)) {
+        validation_errors.push({'key': 'wifigateway' ,'message' : 'You have entered an invalid IP address.'});
+      }
+      if(!validateNetmask(wifi_mask)) {
+        validation_errors.push({'key': 'wifinetmask' ,'message' : 'You have entered an invalid netmask.'});
+      }
+    }
+  }
+
+  if(has_wired == true) {
+    if(wired_dhcp == false) {
+      if(!validateIPaddress(wired_ip)) {
+        validation_errors.push({'key': 'wiredip' ,'message' : 'You have entered an invalid IP address.'});
+      }
+      if(!validateIPaddress(wired_gw)) {
+        validation_errors.push({'key': 'wiredgateway' ,'message' : 'You have entered an invalid IP address.'});
+      }
+      if(!validateNetmask(wired_mask)) {
+        validation_errors.push({'key': 'wirednetmask' ,'message' : 'You have entered an invalid netmask.'});
+      }
+    }
+  }
+
+  // Filling the data for v1 configuration
+
+  if(has_wifi == true) {
+    v1Str += "[wlan0]\r\nssid=" + wifi_ssid + "\r\n";
+    if(wifi_passphrase != "") {
+      v1Str += "passphrase=" + wifi_passphrase + "\r\n";
+    }
+    if(wifi_dhcp == true) {
+      v1Str += "mode=dhcp\r\n";
+    }
+    else {
+      v1Str += "ip=" + wifi_ip + "\r\n";
+      v1Str += "netmask=" + wifi_mask + "\r\n";
+      v1Str += "gateway=" + wifi_gw + "\r\n";
+    }
+    if(wifi_hidden_ssid == true) {
+      v1Str += "hidden_ssid=true" + "\r\n";
+    }
+    v1Str += "\r\n";
+  }
+
+  if(has_wired == true) {
+    v1Str = v1Str + "[eth0]\r\n";
+    if(wired_dhcp == true) {
+      v1Str = v1Str + "mode=dhcp" + "\r\n";
+    }
+    else {
+      v1Str += "ip=" + wired_ip + "\r\n";
+      v1Str += "netmask=" + wired_mask + "\r\n";
+      v1Str += "gateway=" + wired_gw + "\r\n";
+    }
+  }
+
+  if(has_custom_ntp == true || has_custom_dns == true) {
+    v1Str += "\r\n[generic]\r\n";
+  }
+
+  if(has_custom_ntp == true) {
+    v1Str += "ntp=";
+    for(var i = 0; i < ntp_server_list.length; i++) {
+      if (i > 0) {
+        v1Str += ","
+      }
+      v1Str += ntp_server_list[i];
+    }
+    v1Str += "\r\n";
+  }
+
+  if(has_custom_dns == true) {
+    v1Str += "dns=";
+    for(var i = 0; i < dns_server_list.length; i++) {
+      if (i > 0) {
+        v1Str += ","
+      }
+      v1Str += dns_server_list[i];
+    }
+    v1Str += "\r\n";
+  }
+
+  // Filling the data for v2 configuration
+  
+  if(has_wired == true) {
+    var ethConfig = "";
+
+    v2Str += "  ethernets:\r\n";
+    v2Str += "    all-en:\r\n";
+    v2Str += "      match:\r\n";
+    v2Str += "        name: \"en*\"\r\n";
+
+    if(wired_dhcp == true) {
+      ethConfig += "      dhcp4: true\r\n";
+    }
+    else {
+      ethConfig += "      addresses: [" + wired_ip + "/" + convertNetmaskToCIDR(wired_mask) + "]\r\n";
+      ethConfig += "      gateway4: " + wired_gw + "\r\n";
+    }
+
+    if(has_custom_dns == true) {
+      ethConfig += "      nameservers:\r\n"
+      ethConfig += "        addresses: ["
+      for(var i = 0; i < dns_server_list.length; i++) {
+        if (i > 0) {
+          ethConfig += ","
+        }
+        ethConfig += dns_server_list[i];
+      }
+      ethConfig += "]\r\n";
+    }
+
+    v2Str += ethConfig;
+
+    v2Str += "    all-eth:\r\n";
+    v2Str += "      match:\r\n";
+    v2Str += "        name: \"eth*\"\r\n";
+    v2Str += ethConfig;
+  }
+
+  if(has_wifi == true) {
+
+    v2Str += "  wifis:\r\n";
+    v2Str += "    all-wlans:\r\n";
+    v2Str += "      match:\r\n";
+    v2Str += "        name: \"wl*\"\r\n";  // This will match both old (wlan0) and newer (wlp7s0) naming schemas
+
+    v2Str += "      access-points:\r\n";
+    v2Str += "        \"" + wifi_ssid + "\":\r\n";
+
+    if(wifi_passphrase != "" || wifi_hidden_ssid) {
+      v2Str += "          password: \"" + wifi_passphrase + "\"\r\n";
+    }
+
+    if(wifi_dhcp == true) {
+      v2Str += "      dhcp4: true\r\n";
+    }
+    else {
+      v2Str += "      addresses: [" + wifi_ip + "/" + convertNetmaskToCIDR(wifi_mask) + "]\r\n";
+      v2Str += "      gateway4: " + wifi_gw + "\r\n";
+    }
+
+    if(has_custom_dns == true) {
+      v2Str += "      nameservers:\r\n"
+      v2Str += "        addresses: ["
+      for(var i = 0; i < dns_server_list.length; i++) {
+        if (i > 0) {
+          v2Str += ","
+        }
+        v2Str += dns_server_list[i];
+      }
+      v2Str += "]\r\n";
+    }
+  }
 
   var validation_errors_length = validation_errors.length;
   for (var i = 0; i < validation_errors_length; i++) {
@@ -299,10 +415,20 @@ $('#generateconfig').click(function()
 
   if(validation_errors_length == 0)
   {
-    if(configStr.length > 5)
+    if(v1Str.length > 5)
     {
-      alert("Your config file is almost ready!\n\nYour browser will start to download the file as soon as you press OK below. Save the file on to the boot partition of your SD card and make sure to name it network.ini\n\n");
-      saveFile("network.ini", configStr);
+      if($('input[id=screenlyv1]').is(':checked'))
+      {
+        var configStr = v1Str;
+        var configFileName = "network.ini"
+      }
+      else
+      {
+        var configStr = v2Str;
+        var configFileName = "network.yaml"
+      }
+      alert("Your config file is almost ready!\n\nYour browser will start to download the file as soon as you press OK below. Save the file on to the boot partition of your SD card and make sure to name it " + configFileName + "\n\n");
+      saveFile(configFileName, configStr);
     }
     else {
       alert("Please select options before generating configuration");
